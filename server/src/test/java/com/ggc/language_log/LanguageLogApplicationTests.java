01deb5e7f6fe97dc76ca8fce1b3238a47b3350d7
aotan-0404 <aoi2406coma@icloud.com>
2025-06-22 00:19:01 +0900

    feat(test): use H2 in-memory DB for testing via application-test.yml

--- server/src/test/java/com/ggc/language_log/LanguageLogApplicationTests.java
+++ server/src/test/java/com/ggc/language_log/LanguageLogApplicationTests.java
@@ -2,8 +2,10 @@ package com.ggc.language_log;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LanguageLogApplicationTests {
 
 	@Test
