af22c45fbdd45844f2f572bf2f931bb45da79ea8
aotan-0404 <aoi2406coma@icloud.com>
2025-06-21 14:25:12 +0900

    Initial commit

---
+++ server/src/main/java/com/ggc/language_log/LanguageLogApplication.java
@@ -0,0 +1,13 @@
+package com.ggc.language_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLogApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ggc/language_log/LanguageLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggc.language_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
